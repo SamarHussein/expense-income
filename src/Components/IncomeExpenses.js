7fbf8f2acfc68be2fa29ecd44a809ee9f04b177f
Samar Hussein <64905300+SamarHussein@users.noreply.github.com>
2021-01-11 11:49:13 +0100

    Update IncomeExpenses.js

--- src/Components/IncomeExpenses.js
+++ src/Components/IncomeExpenses.js
@@ -5,7 +5,7 @@ export const IncomeExpenses = () => {
     const { transactions } = useContext(GlobalContext);
 
     const amounts = transactions.map(transaction => transaction.amount);
-    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+    //const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
     //const amounts = transa
     const income = amounts
         .filter(item => item > 0)
